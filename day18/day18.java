566c936fa2120a49d269ad0dc8e7ca75bee910e4
JohnCosta27 <johncosta027@gmail.com>
2020-12-18 22:45:30 +0000

    Day 18, not very fast part 2

--- day18/day18.java
+++ day18/day18.java
@@ -1,125 +1,226 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 class day18 {
-
+    
     public static void main(String[] args) {
         
         List<int[]> points = new ArrayList<>();
+        List<int[]> pointsPart2 = new ArrayList<>();
+        
+        char[][] input = {{'.', '.', '#', '.', '.', '#', '.', '.'},
+        {'#', '.', '#', '.', '.', '.', '#', '.'},
+        {'.', '.', '#', '.', '.', '.', '.', '.'},
+        {'#', '#', '.', '.', '.', '.', '#', '#'},
+        {'#', '.', '.', '#', '.', '#', '#', '#'},
+        {'.', '#', '.', '.', '#', '.', '.', '.'},
+        {'#', '#', '#', '.', '.', '#', '.', '.'},
+        {'.', '.', '.', '.', '#', '.', '.', '#'}};
 
-        char[][] input = {{'.', '#', '.'},
-        {'.', '.', '#'},
-        {'#', '#', '#'}};
+        /*char[][] input = {{'.', '#', '.'},
+                          {'.', '.', '#'},
+                          {'#', '#', '#'}};*/
+        
+        //Part 1
         
         //initialise
         for (int i = 0; i < input.length; i++) {
             for (int j = 0; j < input[0].length; j++) {
-                int[] newPoint = {i, j, 0, 0};
-                if (input[j][i] == '#') newPoint[3] = 1;
+                int[] newPoint = {j, i, 0, 0};
+                int[] newPointPart2 = {j, i, 0, 0, 0};
+                if (input[i][j] == '#') {
+                    newPoint[3] = 1;
+                    newPointPart2[4] = 1; 
+                }
                 points.add(newPoint);
+                pointsPart2.add(newPointPart2);
             }
         }
-
-        //points = step(points);
-
+        
+        for (int i = 0; i < 6; i++) {
+            points = step(points);
+            pointsPart2 = stepPart2(pointsPart2);
+        }
+        
         int counter = 0;
         for (int[] p : points) {
             if (p[3] == 1) counter++;
-            System.out.printf("x: %d | y: %d | z: %d | State: %d \n", p[0], p[1], p[2], p[3]);
+            //System.out.printf("x: %d | y: %d | z: %d | State: %d \n", p[0], p[1], p[2], p[3]);
         }
         System.out.println("Part 1: " + counter);
-
+        
+        counter = 0;
+        for (int[] p : pointsPart2) {
+            if (p[4] == 1) counter++;
+            //System.out.printf("x: %d | y: %d | z: %d | State: %d \n", p[0], p[1], p[2], p[3]);
+        }
+        System.out.println("Part 2: " + counter);
+        
     }
     
     static List<int[]> step(List<int[]> points) {
-
-        List<int[]> newPoints = new ArrayList<>();
         
+        List<int[]> newPoints = new ArrayList<>();
+        List<int[]> pointsToCheck = new ArrayList<>();
+
         for (int[] p : points) {
-
+            
             int active = 0;
-
+            
             for (int x = -1; x <= 1; x++) {
                 for (int y = -1; y <= 1; y++) {
                     for (int z = -1; z <= 1; z++) {
                         
                         if (!(x == 0 && y == 0 && z == 0)) {
-    
-                            int[] potentialNeighboor = {p[0] + x, p[1] + y, p[2] + z, 1};
-                            //System.out.printf("x: %d | y: %d | z: %d \n", potentialNeighboor[0], potentialNeighboor[1], potentialNeighboor[2]);
-                            //Checking if potentialNeighboor is in the list, and then checking if its active.
+                            
+                            int[] potentialNeighboor = {p[0] + x, p[1] + y, p[2] + z, 0};
                             int index = contains(potentialNeighboor, points);
                             if (index != -1) {
-                                //System.out.printf("x: %d | y: %d | z: %d \n", potentialNeighboor[0], potentialNeighboor[1], potentialNeighboor[2]);
-                                if (points.get(index)[3] == 1) active++;
+                                int[] foundPoint = points.get(index);
+                                if (foundPoint[3] == 1) {
+                                    active++;
+                                }
+                            } else {
+                                if (contains(potentialNeighboor, pointsToCheck) == -1) pointsToCheck.add(potentialNeighboor);
                             }
-
-                            int active2 = 0;
-
-                            for (int x2 = -1; x <= 1; x++)
-                                for (int y2 = -1; y <= 1; y++)
-                                    for (int z2 = -1; z <= 1; z++) {
-                                        
-                                        int[] potentialNeighboor2 = {potentialNeighboor[0] + x2,
-                                            potentialNeighboor[1] + y2, potentialNeighboor[2] + z2};
-                                        int index2 = contains(potentialNeighboor2, points);
-                                        if (index2 != -1) {
-                                            if (points.get(index)[3] == 1) active2++;
-                                        }
-
-                                    }
-
-                            if (active2 == 3) newPoints.add(potentialNeighboor);
                             
                         }
                         
                     }
                 }
             }
-
-            if ((active == 2 || active == 3) && p[3] == 1) newPoints.add(p);
-            else if ((active < 2 || active > 3) && p[3] == 1) {
-                p[3] = 0;
-                newPoints.add(p);
-            } else if (p[3] == 0 && active == 3) {
-                p[3] = 1;
-                newPoints.add(p);
+            
+            int[] newPoint = {p[0], p[1], p[2], p[3]};
+            
+            if (p[3] == 1) {
+                if (active != 2 && active != 3) newPoint[3] = 0;
             } else {
-                System.out.println("Something else");
+                if (active == 3) newPoint[3] = 1;
             }
-
+            
+            newPoints.add(newPoint);
+            
         }
-
-        return newPoints;
-
-    }
-
-    /*static int activeNeighboors(int[] p) {
         
-        int active = 0;
-
-        for (int x = -1; x <= 1; x++) {
-            for (int y = -1; y <= 1; y++) {
-                for (int z = -1; z <= 1; z++) {
-                    
-                    if (!(x == 0 && y == 0 && z == 0)) {
-
-                        int[] potentialNeighboor = {p[0] + x, p[1] + y, p[2] + z};
-                        //System.out.printf("x: %d | y: %d | z: %d \n", potentialNeighboor[0], potentialNeighboor[1], potentialNeighboor[2]);
-                        int index = contains(potentialNeighboor);
-                        if (index != -1) {
-                            //System.out.printf("x: %d | y: %d | z: %d \n", potentialNeighboor[0], potentialNeighboor[1], potentialNeighboor[2]);
-                            if (points.get(index)[3] == 1) active++;
+        for (int[] p : pointsToCheck) {
+            
+            int active = 0;
+            
+            for (int x = -1; x <= 1; x++) {
+                for (int y = -1; y <= 1; y++) {
+                    for (int z = -1; z <= 1; z++) {
+                        
+                        if (!(x == 0 && y == 0 && z == 0)) {
+                            
+                            int[] potentialNeighboor = {p[0] + x, p[1] + y, p[2] + z, 0};
+                            int index = contains(potentialNeighboor, points);
+                            if (index != -1) {
+                                int[] foundPoint = points.get(index);
+                                if (foundPoint[3] == 1) active++;
+                            }
+                            
                         }
                         
                     }
-                    
                 }
             }
+            
+            int[] newPoint = {p[0], p[1], p[2], p[3]};
+            
+            if (active == 3){
+                newPoint[3] = 1;
+                //System.out.printf("x: %d | y: %d | z: %d | State: %d \n", newPoint[0], newPoint[1], newPoint[2], newPoint[3]);
+            } 
+            
+            newPoints.add(newPoint);
+            
         }
-        return active;
-    }*/
+        
+        return newPoints;
+        
+    }
+    
+    static List<int[]> stepPart2(List<int[]> points) {
+        
+        System.out.println(points.size());
+
+        List<int[]> newPoints = new ArrayList<>();
+        List<int[]> pointsToCheck = new ArrayList<>();
+        
+        for (int[] p : points) {
+            
+            int active = 0;
+
+            for (int x = -1; x <= 1; x++) {
+                for (int y = -1; y <= 1; y++) {
+                    for (int z = -1; z <= 1; z++) {
+                        for (int w = -1; w <= 1; w++) {
+
+                            if (!(x == 0 && y == 0 && z == 0 && w == 0)) {
+                                int[] potentialNeighboor = {p[0] + x, p[1] + y, p[2] + z, p[3] + w, 0};
+                                int index = containsPart2(potentialNeighboor, points);
+                                if (index != -1) {
+                                    int[] foundPoint = points.get(index);
+                                    if (foundPoint[4] == 1) active++;
+                                } else {
+                                    if (containsPart2(potentialNeighboor, pointsToCheck) == -1) pointsToCheck.add(potentialNeighboor);
+                                }
+                                
+                            }
+                            
+                        }
+                    }
+                }
+            }
+            
+            int[] newPoint = {p[0], p[1], p[2], p[3], p[4]};
+            
+            if (p[4] == 1) {
+                if (active != 2 && active != 3) newPoint[4] = 0;
+            } else {
+                if (active == 3) newPoint[4] = 1;
+            }
+            
+            newPoints.add(newPoint);
+            
+        }
+        
+        for (int[] p : pointsToCheck) {
+            
+            int active = 0;
+            
+            for (int x = -1; x <= 1; x++) {
+                for (int y = -1; y <= 1; y++) {
+                    for (int z = -1; z <= 1; z++) {
+                        for (int w = -1; w <= 1; w++) {
+                            if (!(x == 0 && y == 0 && z == 0 && w == 0)) {
+                                
+                                int[] potentialNeighboor = {p[0] + x, p[1] + y, p[2] + z, p[3] + w, 0};
+                                int index = containsPart2(potentialNeighboor, points);
+                                if (index != -1) {
+                                    int[] foundPoint = points.get(index);
+                                    if (foundPoint[4] == 1) active++;
+                                }
+                                
+                            }
+                            
+                        }
+                    }
+                }
+            }
+            
+            int[] newPoint = {p[0], p[1], p[2], p[3], p[4]};
+            
+            if (active == 3) newPoint[4] = 1;
+            newPoints.add(newPoint);
+            
+        }
+        
+        return newPoints;
+        
+    }
     
     static int contains(int[] p, List<int[]> points) {
         for (int[] search : points) {
@@ -129,5 +230,14 @@ class day18 {
         }
         return -1;
     }
-
+    
+    static int containsPart2(int[] p, List<int[]> points) {
+        for (int[] search : points) {
+            if (search[0] == p[0] && search[1] == p[1] && search[2] == p[2] && search[3] == p[3]) {
+                return points.indexOf(search);
+            }
+        }
+        return -1;
+    }
+    
 }
