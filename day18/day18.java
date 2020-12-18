50c8db3151059e545580985b3e13634690a31201
JohnCosta027 <87772@farnborough.ac.uk>
2020-12-18 20:54:40 +0000

    continued day 18

--- day18/day18.java
+++ day18/day18.java
@@ -3,11 +3,11 @@ import java.util.HashMap;
 import java.util.List;
 
 class day18 {
-    
-    static List<int[]> points = new ArrayList<>();
 
     public static void main(String[] args) {
         
+        List<int[]> points = new ArrayList<>();
+
         char[][] input = {{'.', '#', '.'},
         {'.', '.', '#'},
         {'#', '#', '#'}};
@@ -21,14 +21,81 @@ class day18 {
             }
         }
 
+        //points = step(points);
+
+        int counter = 0;
         for (int[] p : points) {
-            System.out.printf("x: %d | y: %d | z: %d \n", p[0], p[1], p[2]);
-            System.out.println(activeNeighboors(p));
+            if (p[3] == 1) counter++;
+            System.out.printf("x: %d | y: %d | z: %d | State: %d \n", p[0], p[1], p[2], p[3]);
         }
+        System.out.println("Part 1: " + counter);
 
     }
     
-    static int activeNeighboors(int[] p) {
+    static List<int[]> step(List<int[]> points) {
+
+        List<int[]> newPoints = new ArrayList<>();
+        
+        for (int[] p : points) {
+
+            int active = 0;
+
+            for (int x = -1; x <= 1; x++) {
+                for (int y = -1; y <= 1; y++) {
+                    for (int z = -1; z <= 1; z++) {
+                        
+                        if (!(x == 0 && y == 0 && z == 0)) {
+    
+                            int[] potentialNeighboor = {p[0] + x, p[1] + y, p[2] + z, 1};
+                            //System.out.printf("x: %d | y: %d | z: %d \n", potentialNeighboor[0], potentialNeighboor[1], potentialNeighboor[2]);
+                            //Checking if potentialNeighboor is in the list, and then checking if its active.
+                            int index = contains(potentialNeighboor, points);
+                            if (index != -1) {
+                                //System.out.printf("x: %d | y: %d | z: %d \n", potentialNeighboor[0], potentialNeighboor[1], potentialNeighboor[2]);
+                                if (points.get(index)[3] == 1) active++;
+                            }
+
+                            int active2 = 0;
+
+                            for (int x2 = -1; x <= 1; x++)
+                                for (int y2 = -1; y <= 1; y++)
+                                    for (int z2 = -1; z <= 1; z++) {
+                                        
+                                        int[] potentialNeighboor2 = {potentialNeighboor[0] + x2,
+                                            potentialNeighboor[1] + y2, potentialNeighboor[2] + z2};
+                                        int index2 = contains(potentialNeighboor2, points);
+                                        if (index2 != -1) {
+                                            if (points.get(index)[3] == 1) active2++;
+                                        }
+
+                                    }
+
+                            if (active2 == 3) newPoints.add(potentialNeighboor);
+                            
+                        }
+                        
+                    }
+                }
+            }
+
+            if ((active == 2 || active == 3) && p[3] == 1) newPoints.add(p);
+            else if ((active < 2 || active > 3) && p[3] == 1) {
+                p[3] = 0;
+                newPoints.add(p);
+            } else if (p[3] == 0 && active == 3) {
+                p[3] = 1;
+                newPoints.add(p);
+            } else {
+                System.out.println("Something else");
+            }
+
+        }
+
+        return newPoints;
+
+    }
+
+    /*static int activeNeighboors(int[] p) {
         
         int active = 0;
 
@@ -52,9 +119,9 @@ class day18 {
             }
         }
         return active;
-    }
+    }*/
     
-    static int contains(int[] p) {
+    static int contains(int[] p, List<int[]> points) {
         for (int[] search : points) {
             if (search[0] == p[0] && search[1] == p[1] && search[2] == p[2]) {
                 return points.indexOf(search);
